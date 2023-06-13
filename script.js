1d472416a01249b20cb874fd2cadd1a75c587c6e
nursedaturkcan123@gmail.com <nursedaturkcan123@gmail.com>
2023-06-13 11:19:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+const API_URL = 'https://api.github.com/users/'
+const form=document.getElementById("form");
+const search=document.getElementById("search");
+const main=document.getElementById("main");
+
+async function getUser(username){
+    try {
+        const {data}=await axios (API_URL+username);
+      
+        createUserCard(data);
+        getRepos(username);
+    } catch (error) {
+      createErrorCard("User not found!");
+    }
+}
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const user=search.value;
+    if(user){
+        getUser(user);
+        search.value="";
+    }
+
+})
+function createUserCard(user){
+const userName=user.name||user.login;
+const userBio=user.bio  ? `<p> ${user.bio}<p/>`: "";
+
+
+    const cardHtml=`
+    <div class="card">
+    <img class="user-image" src=${user.avatar_url}>
+    <div class="user-info">
+        <div class="user-name">
+            <h2>${userName}</h2>
+            <small>@${user.login}</small>
+        </div>
+    </div>
+    <p>${userBio}</p>
+    <ul>
+        <li><i class="fa-solid fa-user-group"></i> ${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li><i class="fa-solid fa-bookmark"></i>${user.public_repos}  <strong>Repository</strong></li>
+    </ul>
+    <div class="repos" id="repos">
+        
+    </div>
+</div>
+    
+    `;
+    main.innerHTML=cardHtml;
+}
+function createErrorCard(msg){
+const cardErrorHtml=`
+<div class="card">
+<h2> ${msg}<h2/>
+<div/>
+
+
+
+`;
+main.innerHTML=cardErrorHtml;
+}
+ async function getRepos(username){
+try {
+    const {data}=await axios (API_URL+username +"/repos");
+    console.log(data);
+    addReposToCard(data)
+} catch (error) {
+    createErrorCard("There is no user repo")
+}
+}
+function addReposToCard(repos){
+    const reposEl=document.getElementById("repos");
+    repos.slice(0,3).forEach(repo => {
+        const reposLink=document.createElement("a");
+        reposLink.href=repo.html_url;
+        reposLink.target="_blank";
+        reposLink.innerHTML=`
+        <i class="fa-solid fa-book-bookmark"></i>${repo.name}
+        `;
+        reposEl.appendChild(reposLink);
+    });
+}
